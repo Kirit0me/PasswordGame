6924ba06286cada46177ba4e67c33f8e15903089
Kiritome <prabhavagarwal444@gmail.com>
2023-09-16 11:37:16 +0530

    time stamp fixed

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
+let guesses = 0;
+const password = "tesseract";
+function getCurrentTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
+}
+
 function getUniqueMessage() {
-    const timestamp = Date.now();
+    const timestamp = getCurrentTime();
     return `Congratulations! You guessed the password in ${guesses} ${guesses === 1 ? 'guess' : 'guesses'}. Timestamp: ${timestamp}`;
 }
 
 const guess = document.getElementById("guess")
 const output = document.getElementById("output")
 
-const password = "tesseract";
 function checkguess(){
-    if(guess === password){
+    const user_guess = guess.value.toLowerCase();
+    guesses++;
+
+    if(user_guess === password){
         unique_message = getUniqueMessage();
         output.innerHTML = unique_message;
         guess.disabled = true;
+    } else {
+        output.innerHTML = 'Skill issue, Try again!'
     }
 }
